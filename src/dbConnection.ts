d08cf319f8886402e07cb8ad67a499b8b9006671
Cassiano Socorro <233652315+CassianoSocorro@users.noreply.github.com>
2025-10-25 13:58:15 -0300

    Finalizando a Estrutura do Projeto

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -1,19 +1,20 @@
-import knex from 'knex';
-import 'dotenv/config'
+import knex from "knex";
+import "dotenv/config";
 
 const connection = knex({
-    client: "mysql",
-    connection: {
-        host: process.env.DB_HOST,
-        port: 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        multipleStatements: true
-    }, pool:{
-      min:2,
-      max:10
-    }
-})
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DPASSWORD,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+});
 
 export default connection;
