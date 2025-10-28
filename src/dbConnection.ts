e1cc3edb89388c8d7255806e59fdc121588f10b2
arthurfinal <arthurfelipe123476@gmail.com>
2025-10-28 14:25:02 -0300

    Atualização do .env

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -1,11 +1,14 @@
 import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
 export const connection = knex({
   client: "pg",
   connection: {
-    host: "localhost",
-    user: "postgres",
-    password: "arthur5432l",
-    database: "trabalhoflavio",
-    port: 5432,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
   },
 });
