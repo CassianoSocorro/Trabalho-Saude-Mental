182a2efe1c72fa2c97e8c6d850be3994ad15b2ac
ArthurFritz8 <geminepestudante5@gmail.com>
2025-11-18 01:01:40 -0300

    estes de erros foram executados e passaram com sucesso

---
+++ tests/errorHandler.test.ts
@@ -0,0 +1,15 @@
+import { throwErrorIfNegative } from '../src/utils/errorHandler';
+
+describe('error handling', () => {
+  test('should throw an error if the number is negative', () => {
+    expect(() => throwErrorIfNegative(-1)).toThrow('Número não pode ser negativo.');
+  });
+
+  test('should not throw an error if the number is positive', () => {
+    expect(throwErrorIfNegative(1)).toBe(1);
+  });
+
+  test('should not throw an error if the number is zero', () => {
+    expect(throwErrorIfNegative(0)).toBe(0);
+  });
+});
