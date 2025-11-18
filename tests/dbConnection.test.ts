eae82342e17cd11f141ea0b6e09c5ad8d72d95d2
ArthurFritz8 <geminepestudante5@gmail.com>
2025-11-18 00:58:22 -0300

    testes assíncronos para a conexão com o banco de dados

---
+++ tests/dbConnection.test.ts
@@ -0,0 +1,13 @@
+import { connection } from '../src/dbConnection';
+
+describe('async & error handling', () => {
+  test('should connect to the database', async () => {
+    const [tables] = await connection.raw('SHOW TABLES');
+    expect(tables).toBeDefined();
+    expect(Array.isArray(tables)).toBe(true);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+});
